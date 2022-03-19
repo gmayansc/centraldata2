fa7cef3701aa4ab0ca0c552af21ea4e2128ac32f
gmayansc <gmayansc@uoc.edu>
2022-03-19 19:50:06 +0100

    app

--- centraldata/src/main/java/edu/uoc/centraldata/controlador/TiendaOnline.java
+++ centraldata/src/main/java/edu/uoc/centraldata/controlador/TiendaOnline.java
@@ -1,9 +1,15 @@
 
 package edu.uoc.centraldata.controlador;
 
+import edu.uoc.centraldata.vista.VentanaMenu;
+
 public class TiendaOnline {
 
     public static void main(String[] args) {
+        //llama a la vista menu
+        //VentanaMenu.iniciarVista();
+        
+        System.out.print("ASDASD");
     }
     
 }
--- centraldata/src/main/java/edu/uoc/centraldata/vista/VentanaMenu.java
+++ centraldata/src/main/java/edu/uoc/centraldata/vista/VentanaMenu.java
@@ -3,6 +3,17 @@ package edu.uoc.centraldata.vista;
 
 public class VentanaMenu {
     
+    public void iniciarVista(){
+        pintarMenu();
+    }
+    
+    public void pintarMenu(){
+        System.out.println("=======================================");
+        System.out.println("=========== TIENDA ONLINE =============");
+        System.out.println("=======================================\n\n");
+        
+    }
+    
 }
 
 //mostrarGestionArticulos(): void
