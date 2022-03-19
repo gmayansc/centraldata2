f76e549f30598180a8276fab6544a5365d5c0e00
mtrivinom <mtrivinom@uoc.edu>
2022-03-19 19:54:21 +0100

    2022-03-19 16-19h
    Todas las funciones completas en las clases: ListaDatos, ListaCliente, ListaArticulo, ListaPedidos.

--- centraldata/src/main/java/edu/uoc/centraldata/modelo/ListaDatos.java
+++ centraldata/src/main/java/edu/uoc/centraldata/modelo/ListaDatos.java
@@ -9,4 +9,33 @@ public ListaDatos() {
     lista = new ArrayList<>();
 }
 
+public int getSize() {
+        return this.lista.size();
+    }
+
+    public void añadir(T t) throws Exception {
+        this.lista.add(t);
+    }
+
+    public void esborrar(T t) {
+        this.lista.remove(t); 
+    }
+    
+    public T getAt(int position) {
+       return this.lista.get(position);
+    }
+
+    public void clear() {
+        this.lista.clear();
+    }
+    
+    public boolean isEmpty() {
+        return lista.isEmpty();
+    }
+    
+    public ArrayList<T> getArrayList() {
+        ArrayList<T> aList = new ArrayList<>(lista);
+        return aList;
+    }
+    
 }
--- centraldata/src/main/java/edu/uoc/centraldata/modelo/ListaPedidos.java
+++ centraldata/src/main/java/edu/uoc/centraldata/modelo/ListaPedidos.java
@@ -1,5 +1,87 @@
 package edu.uoc.centraldata.modelo;
 
-public class ListaPedidos extends ListaDatos<Pedido>{
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
 
+public class ListaPedidos<T> extends ListaDatos<Pedido>{
+    
+    public Pedido Pedido;
+    public Cliente Cliente;
+    public Articulo Articulo;
+            
+    public ArrayList<T> listaPedido;
+    
+    private void inicializarValores() {
+        listaPedido.add ((T) new Pedido("34", "pepeluis@uoc.edu", "1234", 1, "2022-03-19", "18:30", 1.65, false));
+        listaPedido.add ((T) new Pedido("5", "marianito@gmail.com", "45", 4, "2022-02-28", "07:52", 11.04, true));
+    }
+
+public boolean existePedido(String Numero) {
+        boolean bol = false;
+        for (int i = 0; i < listaPedido.size(); i++) {
+            Pedido = (edu.uoc.centraldata.modelo.Pedido) listaPedido.get(i);
+            if (Numero.equals(Pedido.getNumero())) {
+                bol= true;
+            } else {
+                bol= false;
+            }
+        }
+        return bol;
+    }
+    
+    public boolean existePedido(Pedido Pedido) {
+        throw new UnsupportedOperationException(""); 
+    }
+    
+    public void agregarPedido(Pedido Pedido) {
+        if (!this.existePedido(Pedido)) {
+            this.listaPedido.add((T) Pedido);
+            System.out.println("El Pedido ha sido Creado");
+        } else {
+            System.out.println("Este Pedido ya existe");
+        }
+    }
+    
+    public void eliminarPedido(Pedido Pedido) {
+        if (existePedido(Pedido.getNumero()) == true) {
+            this.listaPedido.remove((T) Pedido);
+            System.out.println("El Pedido ha sido Articulo");
+        } else {
+            System.out.println("No existe el Pedido");
+        }
+    }
+     
+    public void listarPedido(Pedido Pedido) {
+        if (existePedido(Pedido.getNumero()) == true) {
+            System.out.println("Listado de Pedidos (" + this.Pedido.getNumero() + "):");
+        } else {
+            System.out.println("No existe el Pedido");
+        }    
+    }
+    
+   public boolean comprobarEnvio(boolean Tipo) {
+        boolean bol = false;
+        for (int i = 0; i < listaPedido.size(); i++) {
+            Pedido = (edu.uoc.centraldata.modelo.Pedido) listaPedido.get(i);
+        }
+        return bol;
+    }
+    
+    public void listarPedidosPendientes() {
+        if (comprobarEnvio(Pedido.getEnvio()) == false) {
+            System.out.println("Listado de Pedidos Enviados (" + this.Pedido.getEnvio() + "):");
+        } else {
+            System.out.println("No existen Pedidos");
+        } 
+    }
+    
+    public void listarPedidosEnviados() {
+        if (comprobarEnvio(Pedido.getEnvio()) == true) {
+            System.out.println("Listado de Pedidos Enviados (" + this.Pedido.getEnvio() + "):");
+        } else {
+            System.out.println("No existen Pedidos");
+        }    
+    }
+    
 }
